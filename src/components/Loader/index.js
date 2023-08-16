94da4b209af4273e346dec288881ee2bb4016cce
rodrigorunner <rodrigoregisrun@gmail.com>
2023-08-16 09:25:58 -0300

    media querie added

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,35 +1,31 @@
 import { MagnifyingGlass } from "react-loader-spinner"
+import "./loader.css"
 
 const Loader = () => {
     return(
-        <div style={{
-            position: 'absolute',
-            top:'45%',
-            left: '30%'
-        }} className="container">
+        <div className="container">
             <div className="row">
-                <div className="col-md-6 mx-auto">
-                <MagnifyingGlass
-                visible={true}
-                height="80"
-                width="80"
-                ariaLabel="MagnifyingGlass-loading"
-                wrapperStyle={{}}
-                wrapperClass="MagnifyingGlass-wrapper"
-                glassColor = '#c0efff'
-                color = '#e15b64'
-                />
-                 <MagnifyingGlass
-                visible={true}
-                height="80"
-                width="80"
-                ariaLabel="MagnifyingGlass-loading"
-                wrapperStyle={{}}
-                wrapperClass="MagnifyingGlass-wrapper"
-                glassColor = '#c0efff'
-                color = '#e15b64'
-                />
-                
+                <div className="loader col-sm-12 col-md-6 mx-auto">
+                    <MagnifyingGlass
+                    visible={true}
+                    height="80"
+                    width="80"
+                    ariaLabel="MagnifyingGlass-loading"
+                    wrapperStyle={{}}
+                    wrapperClass="MagnifyingGlass-wrapper"
+                    glassColor = '#c0efff'
+                    color = '#e15b64'
+                    />
+                    <MagnifyingGlass
+                    visible={true}
+                    height="80"
+                    width="80"
+                    ariaLabel="MagnifyingGlass-loading"
+                    wrapperStyle={{}}
+                    wrapperClass="MagnifyingGlass-wrapper"
+                    glassColor = '#c0efff'
+                    color = '#e15b64'
+                    />               
                 </div>
             </div>
         </div>
